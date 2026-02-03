94182156aede4ac1278366bae1572e9a26f559ce
Dana Kun <dana.liepinsh@gmail.com>
2026-02-03 10:06:32 +0100

    hotfix footer padding

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,10 +5,7 @@ import { TransitionLink } from "./TransitionLink";
 
 export default function Footer() {
   return (
-    <footer className="mx-auto max-w-[1400] bg-white pt-6 pb-6">
-      <div className="px-5">
-        <div className="divider border-b border-black/50"></div>
-      </div>
+    <footer className="mx-auto max-w-[1400] bg-white pt-16 pb-6">
       <Grid className="px-5">
         <Column span={2} spanMd={2} spanSm={2} className="pt-5 pb-7">
           {/* About Section */}
